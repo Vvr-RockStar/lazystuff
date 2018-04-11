7ffecc3e9d27ba95e96c604668ba74727b89e839
Vishnu vardhan reddy <thisisvvr@gmail.com>
2018-04-11 20:07:11 +0530

    Use this for cloning device specific blobs

--- device.sh
+++ device.sh
@@ -1,1 +1,3 @@
-
+git clone https://github.com/abhirajsinghofficial/android_vendor_xiaomi_santoni -b los-15.1 vendor/xiaomi
+git clone https://github.com/abhirajsinghofficial/android_device_xiaomi_santoni.git -b aim device/xiaomi/santoni
+git clone https://github.com/abhirajsinghofficial/android_device_xiaomi_santoni.git -b lineage-15.1-santoni kernel/xiaomi/msm8937
