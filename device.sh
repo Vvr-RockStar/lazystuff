413d5decc84431b90e68e3969bf168304638470c
Vishnu vardhan reddy <thisisvvr@gmail.com>
2018-04-12 08:45:52 +0530

    Update device.sh

--- device.sh
+++ device.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/abhirajsinghofficial/android_vendor_xiaomi_santoni -b los-15.1 vendor/xiaomi
 git clone https://github.com/abhirajsinghofficial/android_device_xiaomi_santoni.git -b aim device/xiaomi/santoni
-git clone https://github.com/abhirajsinghofficial/android_device_xiaomi_santoni.git -b lineage-15.1-santoni kernel/xiaomi/msm8937
+git clone https://github.com/Vvr-RockStar/android_kernel_xiaomi_msm8937.git -b lineage-15.1-santoni kernel/xiaomi/msm8937
