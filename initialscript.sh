c7d9b8120db58dd09d2b563605d41dc9978ee397
Vishnu vardhan reddy <thisisvvr@gmail.com>
2018-05-18 22:02:17 +0530

    Update initialscript.sh
    remove some stuff

--- initialscript.sh
+++ initialscript.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt upgrade -y && sudo apt-get install git-core &&  git clone https://github.com/akhilnarang/scripts && cd scripts && bash setup/ubuntu1604linuxmint18.sh &&  mkdir -p ~/bin && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
+git clone https://github.com/akhilnarang/scripts && cd scripts && bash setup/ubuntu1604linuxmint18.sh &&  mkdir -p ~/bin && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
 
 git clone https://github.com/abhirajsinghofficial/android_vendor_xiaomi_santoni -b los-15.1 vendor/xiaomi
 git clone https://github.com/abhirajsinghofficial/android_device_xiaomi_santoni.git -b aim device/xiaomi/santoni
