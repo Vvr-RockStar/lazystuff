6c6b8b1e36782d22fadfcb14e0cbedf5758d9f8f
Vishnu vardhan reddy <thisisvvr@gmail.com>
2018-05-18 22:08:04 +0530

    Update initialscript.sh

--- initialscript.sh
+++ initialscript.sh
@@ -1,8 +1,1 @@
 git clone https://github.com/akhilnarang/scripts && cd scripts && bash setup/ubuntu1604linuxmint18.sh &&  mkdir -p ~/bin && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
-
-git clone https://github.com/abhirajsinghofficial/android_vendor_xiaomi_santoni -b los-15.1 vendor/xiaomi
-git clone https://github.com/abhirajsinghofficial/android_device_xiaomi_santoni.git -b aim device/xiaomi/santoni
-git clone https://github.com/andreisaniuk/android_kernel_xiaomi_msm8937.git -b lineage-15.1 kernel/xiaomi/msm8937
-
-cd device/xiaomi/santoni
-nano aim.mk
